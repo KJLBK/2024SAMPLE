fb03072d40f01e6574924bcb282420c7bc625455
rdyjun <rdyjun00@gmail.com>
2024-03-08 17:18:30 +0900

    feat: join 메소드 반환 타입 변경
    boolean 타입에서 Member로 변경

--- sungjun/src/main/java/demo/service/member/RegisterService.java
+++ sungjun/src/main/java/demo/service/member/RegisterService.java
@@ -1,10 +1,11 @@
 package demo.service.member;
 
+import demo.entity.member.Member;
 import demo.entity.member.RegisterDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface RegisterService {
 
-	public boolean join(RegisterDto registerDTO);
+	public Member join(RegisterDto registerDTO);
 }
