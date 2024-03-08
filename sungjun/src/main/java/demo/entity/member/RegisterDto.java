29724a80501605d84852c5aab967f60a78be45a1
rdyjun <rdyjun00@gmail.com>
2024-03-08 17:15:08 +0900

    feat: 회원가입 DTO 추가
    id, pw, name, phone 속성

---
+++ sungjun/src/main/java/demo/entity/member/RegisterDto.java
@@ -0,0 +1,15 @@
+package demo.entity.member;
+
+import lombok.Getter;
+
+@Getter
+public class RegisterDto {
+
+	private String id;
+
+	private String pw;
+
+	private String name;
+
+	private String phone;
+}
