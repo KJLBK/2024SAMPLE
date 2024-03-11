74bee9b28624908fe086df90a75f1774b7987ec3
KIMWOOBIN <woodol4540@naver.com>
2024-03-11 14:22:44 +0900

    Day0311

--- woobin/src/main/java/com/example/demo/repository/JpaMemberRepository.java
+++ woobin/src/main/java/com/example/demo/repository/JpaMemberRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JpaMemberRepository extends JpaRepository<Member, String> {
-
+    Optional<Member> findByMemberEmail(String memberEmail);
 }
